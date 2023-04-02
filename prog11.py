d121dcc9633794a56e35590e503db7bed815c434
kaviyamalya <123438995+kaviyamalya@users.noreply.github.com>
2023-04-03 01:11:02 +0530

    sort a map by value / Key

---
+++ prog11.py
@@ -0,0 +1,13 @@
+##Implement a program to sort a map by value / Key
+
+
+my_dict = {'apple': 10, 'orange': 5, 'banana': 20}
+
+
+sorted_dict = dict(sorted(my_dict.items()))
+
+
+print("Sorted dictionary by key:", sorted_dict)
+
+
+
