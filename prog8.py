e9de0d7751151460f6eaff6386116165efb005cf
kaviyamalya <123438995+kaviyamalya@users.noreply.github.com>
2023-04-02 23:49:51 +0530

    Create prog8.py

---
+++ prog8.py
@@ -0,0 +1,21 @@
+##Python program to determine whether the given number is a Harshad Number
+
+def is_harshad_number(n):
+    
+    digits = list(str(n))
+    
+    
+    digit_sum = sum([int(d) for d in digits])
+    
+   
+    if n % digit_sum == 0:
+        return True
+    else:
+        return False
+
+
+number = 20
+if is_harshad_number(number):
+    print(f"{number} is a Harshad Number")
+else:
+    print(f"{number} is not a Harshad Number")
